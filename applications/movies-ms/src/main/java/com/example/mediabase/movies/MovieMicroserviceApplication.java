a858332692e96bd6652b7cdf18eb5c4a6981d4b7
PrftBC <37965519+PrftBC@users.noreply.github.com>
2019-07-31 12:20:18 -0400

    at end og SD

--- applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
+++ applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
@@ -2,9 +2,10 @@ package com.example.mediabase.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@SpringBootApplication
+@SpringBootApplication @EnableEurekaClient
 public class MovieMicroserviceApplication {
 
     public static void main(String... args) {
